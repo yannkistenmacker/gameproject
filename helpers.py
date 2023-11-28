0a78237ad6eabfcda63d348a678fb6f2ba111646
Yann <77469325+yannkistenmacker@users.noreply.github.com>
2023-11-28 16:01:27 -0300

    Integrando biblioteca flaskwtf para validar forms

--- helpers.py
+++ helpers.py
@@ -1,8 +1,24 @@
 import os
 from jogoteca import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, SubmitField
+
+
+class FormularioJogo(FlaskForm):
+    nome = StringField('Nome do Jogo', [validators.DataRequired(),  validators.Length(min=1, max=50)])
+    categoria = StringField('Categoria', [validators.DataRequired(),  validators.Length(min=1, max=40)])
+    console = StringField('Console', [validators.DataRequired(),  validators.Length(min=1, max=50)])
+    salvar = SubmitField('Salvar')
+
 
 def recupera_imagem(id):
     for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
-        if f'capa{id}.jpg' == nome_arquivo:
+        if f'capa{id}' in nome_arquivo:
             return nome_arquivo
     return 'capa_padrao.jpg'
+
+
+def deleta_arquivo(id):
+    arquivo = recupera_imagem(id)
+    if arquivo != 'capa_padrao.jpg':
+        os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
-from helpers import recupera_imagem
+from helpers import recupera_imagem, deleta_arquivo, FormularioJogo
 import time
 
 
@@ -16,14 +16,20 @@ def index():
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] is None:
         return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='Novo Jogo')
+    form = FormularioJogo()
+    return render_template('novo.html', titulo='Novo Jogo', form=form)
 
 
 @app.route('/criar', methods=['POST', ])
 def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
+    form = FormularioJogo(request.form)
+
+    if not form.validate_on_submit():
+        return redirect(url_for('novo'))
+
+    nome = form.nome.data
+    categoria = form.categoria.data
+    console = form.console.data
 
     jogo = Jogos.query.filter_by(nome=nome).first()
     if jogo:
@@ -50,7 +56,8 @@ def editar(id):
     capa_jogo = recupera_imagem(id)
     return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=capa_jogo)
 
-#Criando rota para alterar as informacoes no banco de dados a partir da rota editar
+
+# Criando rota para alterar as informacoes no banco de dados a partir da rota editar
 @app.route('/atualizar', methods=['POST', ])
 def atualizar():
     jogo = Jogos.query.filter_by(id=request.form['id']).first()
@@ -61,10 +68,11 @@ def atualizar():
     db.session.add(jogo)
     db.session.commit()
 
-# Trecho de codigo para alterar imagem de capa
+    # Trecho de codigo para alterar imagem de capa
     arquivo = request.files['arquivo']
     upload_path = app.config['UPLOAD_PATH']
     timestamp = time.time()
+    deleta_arquivo(id)
     arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
 
     return redirect(url_for('index'))
@@ -79,6 +87,7 @@ def deletar(id):
     db.session.commit()
     flash('Jogo deletado com sucesso')
     return redirect(url_for('index'))
+
 
 # Criando rota para a pagina de login
 @app.route('/login')
@@ -107,8 +116,8 @@ def logout():
     flash('Logout efetuado com sucesso!')
     return redirect(url_for('index'))
 
+
 # Rota para upload de arquivo
 @app.route('/uploads/<nome_arquivo>')
 def imagem(nome_arquivo):
     return send_from_directory('uploads', nome_arquivo)
-
