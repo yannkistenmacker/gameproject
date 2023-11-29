abacf5c6bdadb5533eeb63d6acf1847f3bac7b70
Yann <77469325+yannkistenmacker@users.noreply.github.com>
2023-11-29 16:32:40 -0300

    Upload arquivo rota user

---
+++ views_user.py
@@ -0,0 +1,36 @@
+from jogoteca import app
+from flask import render_template, request, redirect, session, flash, url_for
+from models import Usuarios
+from helpers import FormularioUsuario
+from flask_bcrypt import check_password_hash
+
+
+# Criando rota para a pagina de login
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    form = FormularioUsuario()
+    return render_template('login.html', proxima=proxima, form=form)  # Criando variavel proxima
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    form = FormularioUsuario(request.form)
+
+    usuario = Usuarios.query.filter_by(nickname=form.nickname.data).first()
+    senha = check_password_hash(usuario.senha, form.senha.data)
+    if usuario and senha:
+        session['usuario_logado'] = usuario.nickname
+        flash(usuario.nickname + ' logado com sucesso!')
+        #proxima_pagina = request.form['proxima']
+        return redirect('/')
+    else:
+        flash('Usuario nao logado!')
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
