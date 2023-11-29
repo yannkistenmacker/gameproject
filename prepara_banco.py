26292e644ef2af859666f44b9f3df7aaef8ee6e6
Yann <77469325+yannkistenmacker@users.noreply.github.com>
2023-11-29 16:46:58 -0300

    Update prepara_banco.py

--- prepara_banco.py
+++ prepara_banco.py
@@ -7,7 +7,7 @@ try:
     conn = mysql.connector.connect(
         host='127.0.0.1',
         user='root',
-        password='Tryh4ckm3.'
+        password='root'
     )
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
