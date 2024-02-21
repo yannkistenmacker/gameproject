74a0b097b0f1f3539d1822ae6dcf6196d0252d82
Yann <77469325+yannkistenmacker@users.noreply.github.com>
2024-02-21 17:10:09 -0300

    alterando a chamada app.run

--- jogoteca.py
+++ jogoteca.py
@@ -1,15 +1,17 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
+bcrypt = Bcrypt(app)
 
-from views import *
+from views_game import *
+from views_user import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
